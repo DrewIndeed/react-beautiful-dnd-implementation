864e9d48706877e7e4e7cf5ee8b554474e02d164
Andrew Le <s3820098@rmit.edu.vn>
2022-03-11 22:34:57 +0700

    8. Step 4: Persist List Reordering

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,52 @@ const App = () => {
   const [initState, setInitState] = useState(initialData);
 
   const onDragEnd = (result) => {
-    // TODO: render column
+    // destructure result object
+    const { draggableId, source, destination } = result;
+
+    // check if destination is null (user drop it outside a column or so, ...)
+    if (!destination) return;
+
+    // check if user drops at the same point
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    )
+      return;
+
+    // retrieve the working-on column from the state
+    const workingOnColumn = initState.columns[source.droppableId];
+
+    // retrieve the working-on task ids array of the above column from the state
+    const newTaskIdsContainer = [...workingOnColumn.taskIds];
+
+    // remove one item to start reordering
+    newTaskIdsContainer.splice(source.index, 1);
+
+    // insert at the new position
+    newTaskIdsContainer.splice(destination.index, 0, draggableId);
+
+    // create new column
+    const newColumn = {
+      ...workingOnColumn,
+      taskIds: newTaskIdsContainer,
+    };
+
+    // create a new state object before updating the state
+    const newState = {
+      ...initState,
+      columns: {
+        ...initState.columns,
+        [newColumn.id]: newColumn,
+      },
+    };
+
+    // using setState to update the state
+    setInitState(newState);
   };
 
   return (
-    <DragDropContext onDragEnd={() => onDragEnd()}>
+    <DragDropContext onDragEnd={onDragEnd}>
       {initState.columnOrder.map((colId) => {
         // use id of column to ensure the columns order
         const columnObjectOfCurrentId = initState.columns[colId];
