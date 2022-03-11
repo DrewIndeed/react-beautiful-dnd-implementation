be2ed478ab7adff981282dd81aec735b20623873
Andrew Le <s3820098@rmit.edu.vn>
2022-03-11 22:10:35 +0700

    5. Step 1: Add DragDropContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,36 @@
 import React, { useState } from 'react';
 import initialData from './initialData';
 import Column from './Column';
+import { DragDropContext } from 'react-beautiful-dnd';
 
 const App = () => {
   const [initState, setInitState] = useState(initialData);
-  return initState.columnOrder.map((colId) => {
-    // use id of column to ensure the columns order
-    const columnObjectOfCurrentId = initState.columns[colId];
 
-    // use id of task of above column to ensure the tasks order
-    const tasksListOfCurrentIdColumn = columnObjectOfCurrentId.taskIds.map(
-      (taskId) => initState.tasks[taskId]
-    );
+  const onDragEnd = (result) => {
+    // TODO: render column
+  };
 
-    return (
-      <Column
-        key={columnObjectOfCurrentId.id}
-        column={columnObjectOfCurrentId}
-        tasks={tasksListOfCurrentIdColumn}
-      />
-    );
-  });
+  return (
+    <DragDropContext onDragEnd={() => onDragEnd()}>
+      {initState.columnOrder.map((colId) => {
+        // use id of column to ensure the columns order
+        const columnObjectOfCurrentId = initState.columns[colId];
+
+        // use id of task of above column to ensure the tasks order
+        const tasksListOfCurrentIdColumn = columnObjectOfCurrentId.taskIds.map(
+          (taskId) => initState.tasks[taskId]
+        );
+
+        return (
+          <Column
+            key={columnObjectOfCurrentId.id}
+            column={columnObjectOfCurrentId}
+            tasks={tasksListOfCurrentIdColumn}
+          />
+        );
+      })}
+    </DragDropContext>
+  );
 };
 
 export default App;
