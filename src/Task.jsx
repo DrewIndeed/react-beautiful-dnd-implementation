ac9e71741dfbdeb555abc375061a75d620f93dca
Andrew Le <s3820098@rmit.edu.vn>
2022-03-11 22:11:06 +0700

    7. Step3: add Draggable

--- src/Task.jsx
+++ src/Task.jsx
@@ -1,15 +1,29 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Draggable } from 'react-beautiful-dnd';
 
 const Container = styled.div`
   margin: 8px;
   border: 2px solid red;
   border-radius: 5px;
   padding: 8px;
+  background-color: white;
 `;
 
-const Task = ({ task }) => {
-  return <Container>{task.content}</Container>;
+const Task = ({ task, index }) => {
+  return (
+    <Draggable draggableId={task.id} index={index}>
+      {(provided) => (
+        <Container
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          ref={provided.innerRef}
+        >
+          {task.content}
+        </Container>
+      )}
+    </Draggable>
+  );
 };
 
 export default Task;
